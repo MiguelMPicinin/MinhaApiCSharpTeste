823d1590205476fab9650a481578921c1504ec63
MiguelMPicinin <miguelpicinin6@gmail.com>
2026-02-25 13:01:52 -0300

    Add project files.

---
+++ MinhaApi/Controllers/ProdutosController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaApi.Models;
+
+namespace MinhaApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private static readonly List<Produto> _produtos = new();
+
+    [HttpGet]
+    public IActionResult Get() => Ok(_produtos);
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var produto = _produtos.FirstOrDefault(p => p.Id == id);
+        if (produto == null) 
+            return NotFound();
+        return Ok(produto);
+    }
+
+    [HttpPost]
+    public IActionResult Post(Produto produto)
+    {
+        produto.Id = _produtos.Count + 1;
+        _produtos.Add(produto);
+        return CreatedAtAction(nameof(Get), new { id = produto.Id }, produto);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, Produto produtoAtualizado)
+    {
+        var produto = _produtos.FirstOrDefault(p => p.Id == id);
+        if (produto == null)
+            return NotFound();
+
+        produto.Nome = produtoAtualizado.Nome;
+        produto.Preco = produtoAtualizado.Preco;
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var produto = _produtos.FirstOrDefault(p => p.Id == id);
+        if (produto == null)
+            return NotFound();
+
+        _produtos.Remove(produto);
+        return NoContent();
+    }
+}
